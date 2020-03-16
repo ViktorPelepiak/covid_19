f8443a23a13e41fd985553ea5c0c318fcd9fcbff
ViktorPelepiak <viktorpelepiak@gmail.com>
2020-03-16 08:37:40 +0200

    Added @Repository annotations

--- src/main/java/com/covid/repository/PatientRepository.java
+++ src/main/java/com/covid/repository/PatientRepository.java
@@ -2,7 +2,9 @@ package com.covid.repository;
 
 import com.covid.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
 }
